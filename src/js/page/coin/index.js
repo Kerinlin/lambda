2251ecb9280f5256dac7c115dc96671ab60e5128
kerinlin <18296613547@qq.com>
2018-10-10 18:21:48 +0800

    新增页面

---
+++ src/js/page/coin/index.js
@@ -0,0 +1,22 @@
+require("../../../css/lib/reset.css");
+require("../../../css/common/font.less");
+require("../../../css/common/global.less");
+require("../../../css/common/main.less");
+require("../../../css/common/grid.less");
+require("../../../css/common/header.less");
+require("../../../css/common/footer.less");
+require("../../../css/page/index.less");
+require("../../../css/page/coin.less");
+require("../../../css/common/useful.less")
+//引入 js
+require("../../components/jquery-confirm/jquery-confirm.js");
+require("../../components/jquery-confirm/jquery-confirm.less");
+require("../../../js/components/header.js");
+import Clipboard from "clipboard";
+$(document).ready(function() {
+  var clipboard = new Clipboard("#btnCopy");
+  clipboard.on("success", function(e) {
+    $.alert("内容已复制到剪贴板！");
+    e.clearSelection();
+  });
+});
